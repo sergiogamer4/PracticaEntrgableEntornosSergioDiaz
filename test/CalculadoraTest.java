a05e0756d8f091b4c2fbe8e438bf80dde26dcbf7
sergi <sergiogamer4sdg@gmail.com>
2024-03-13 23:05:31 +0100

    Se añade la pila de pruebas de restar

--- test/CalculadoraTest.java
+++ test/CalculadoraTest.java
@@ -37,6 +37,23 @@ class CalculadoraTest {
 
     @Test
     void restar() {
+        double valor1 = 6.000;
+        double valor2 = 14.000;
+        double expResult = -8.000;
+        double result = Calculadora.restar(valor1, valor2);
+        assertEquals(expResult, result);
+
+        double valor3 = 247.345;
+        double valor4 = 1.290;
+        double expResult2 = 246.055;
+        double result2 = Calculadora.restar(valor3, valor4);
+        assertEquals(expResult2, result2);
+
+        double valor5 = 0.230;
+        double valor6 = 50.000;
+        double expResult3 = -49.770;
+        double result3 = Calculadora.restar(valor5, valor6);
+        assertEquals(expResult3, result3);
     }
 
     @Test
